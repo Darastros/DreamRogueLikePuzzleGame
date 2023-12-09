96bb50977e105f20ec80eb04bb3b09fe7111eaae
Becher <mbelkhenchir@gmail.com>
2023-12-09 12:39:59 +0100

    Destroy objects at absorption

--- Assets/Scripts/CardGameControllers/CardObject.cs
+++ Assets/Scripts/CardGameControllers/CardObject.cs
@@ -8,23 +8,36 @@ namespace CardGame
     public class CardObject : MonoBehaviour
     {
         [SerializeField] private Card m_card;
+        private Animator m_animator;
         
         private Action<Card> m_callback;
         public CardType Type => m_card.Type;
 
+        public void Awake()
+        {
+            m_animator = GetComponent<Animator>();
+        }
+
         public void StartAbsorb(Action<Card> _callback)
         {
             m_callback = _callback;
+            m_animator.SetBool("Absorb", true);
         }
 
         public void StopAbsorb()
         {
             m_callback = null;
+            m_animator.SetBool("Absorb", false);
         }
 
         private void Absorb()
         {
             m_callback?.Invoke(m_card);
+        }
+
+        public void Destroy()
+        {
+            Destroy(gameObject);
         }
     }
 }
