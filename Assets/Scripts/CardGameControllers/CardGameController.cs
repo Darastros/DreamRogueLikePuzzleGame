a3dbbe66de69e0e467b12357b0b023efe30ca29f
Becher <mbelkhenchir@gmail.com>
2023-12-09 13:07:30 +0100

    Add hand to collect cards

--- Assets/Scripts/CardGameControllers/CardGameController.cs
+++ Assets/Scripts/CardGameControllers/CardGameController.cs
@@ -7,14 +7,32 @@ namespace CardGame
     public class CardGameController : MonoBehaviour
     {
         [SerializeField] private CardDetector m_detector;
+        [SerializeField] private int m_maxCardsInHand = 5;
+        [SerializeField] private List<Card> m_hand;
         
-        void Start()
+        void OnEnable()
+        {
+            ListenEvent();
+            m_hand = new List<Card>();
+        }
+
+        void OnDisable()
+        {
+            UnListenEvent();
+        }
+
+        private void ListenEvent()
         {
             m_detector.OnCardObjectEnter += EnterCard;
             m_detector.OnCardObjectExit += ExitCard;
-            
         }
-
+        
+        private void UnListenEvent()
+        {
+            m_detector.OnCardObjectEnter -= EnterCard;
+            m_detector.OnCardObjectExit -= ExitCard;
+        }
+        
         private void EnterCard(CardObject _card)
         {
             Debug.Log("Enter " + _card.Type);
@@ -30,6 +48,8 @@ namespace CardGame
         void AbsorbCard(Card _card)
         {
             Debug.Log("Absorb card " + _card.Type);
+            m_hand.Add(_card);
+            if(m_hand.Count > m_maxCardsInHand) m_hand.RemoveAt(0);
         }
     }
 }
