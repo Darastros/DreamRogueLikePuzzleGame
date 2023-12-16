7cb82db8ecca2841ded5e3f2d30dcd6c16e56927
Becher <mbelkhenchir@gmail.com>
2023-12-16 07:54:14 +0100

    Spawner

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,6 +6,7 @@ public class Spawner : MonoBehaviour
 {
     [SerializeField] private float m_spawnFrequency = 1.0f;
     [SerializeField] private GameObject m_spawnObject;
+    [SerializeField] private float m_speed = 10.0f;
     private Animator m_animator;
     private float m_timer;
     
@@ -20,12 +21,21 @@ public class Spawner : MonoBehaviour
         m_timer -= Time.deltaTime;
         if (m_timer <= 0.0f)
         {
-            
+            if (m_animator)
+            {
+                m_animator.SetTrigger("Spawn");
+            }
+            else Spawn();
+            m_timer = m_spawnFrequency;
         }
     }
 
     public void Spawn()
     {
-        
+        var instance = Instantiate(m_spawnObject, transform.position, transform.rotation);
+        if (instance.TryGetComponent(out Rigidbody2D _rigidbody))
+        {
+            _rigidbody.velocity = transform.right * m_speed;
+        }
     }
 }
