797d6e43ef23d86c914217f86e25e0a2924723f1
Guilhem PECH <guilhem.pech@gmail.com>
2023-12-16 16:59:04 +0100

    Change timer and timer exposition of worm

--- Assets/Scripts/GameSystems/RoomScript/StartRoom.cs
+++ Assets/Scripts/GameSystems/RoomScript/StartRoom.cs
@@ -11,7 +11,10 @@ namespace GameSystems.RoomScript
 
         private void OnDestroy()
         {
-            GameManager.Instance.RemoveStartGameFromStack();
+            if (GameManager.Instance)
+            {
+                GameManager.Instance.RemoveStartGameFromStack();
+            }
         }
     }
 }
